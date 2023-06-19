303f19c0f83ebda58efa47eb4fcef04d81fe6413
Anna K <akuroshenkova@gmail.com>
2023-06-19 16:36:41 +0100

    Lab 201 06

---
+++ sales.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const container = document.getElementById("container");
+
+const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
+
+
+const seattle = {
+  storeName: "seattle",
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesPerHour: 6.3,
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalDailyCookies: 0,
+
+  calcCustomersEachHour: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.customersEachHour.push(randomNum(this.minCustPerHour, this.maxCustPerHour));
+    }
+    console.log(this.customersEachHour);
+  },
+  calcCookiesEachHour: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerHour);
+      this.cookiesEachHour.push(oneHour);
+      this.totalDailyCookies += oneHour;
+    }
+  },
+  render: function () {
+    this.calcCustomersEachHour();
+    this.calcCookiesEachHour();
+
+  
+    const article = document.createElement("article");
+    container.appendChild(article);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = this.storeName;
+    article.appendChild(h3);
+
+    const ul = document.createElement("ul");
+    article.appendChild(ul);
+
+    
+    for (let i = 0; i < hours.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`;
+      ul.appendChild(li);
+      
+    }
+    
+
+    const h4 = document.createElement('h4');
+    h4.textContent = `Total ${this.totalDailyCookies} cookies`;
+    article.appendChild(h4);
+  },
+
+  
+}
+seattle.render();
+
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
